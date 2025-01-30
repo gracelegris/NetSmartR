8f046f8a0fd77840585d49e95aae0a513517b262
Grace Legris <gracebea@gmail.com>
2025-01-30 14:43:57 -0600

    trying to read in settlement blocks from public google drive folder instead of dropbox

--- R/est_buildingheightpop.R
+++ R/est_buildingheightpop.R
@@ -15,6 +15,7 @@ est_buildingheightpop <- function(building_data_path, settlement_data_path, ward
   # read in settlement blocks, building data, and ward shapefile and transform to same crs
   #message("Loading settlement blocks for ", ward, ", ", state)
   # read in settlement blocks data
+  message("Loading settlement blocks data for ", ward, ", ", state)
   settlement_blocks <- st_read(settlement_data_path) %>%
     filter(state == state, landuse == 'Residential')
 
@@ -30,11 +31,11 @@ est_buildingheightpop <- function(building_data_path, settlement_data_path, ward
     settlement_blocks <- settlement_blocks[st_is_valid(settlement_blocks), ]
   }
 
-  #message("Loading building footprints for ", ward, ", ", state)
+  message("Loading building footprints for ", ward, ", ", state)
   building_data <- st_read(building_data_path)  %>%
     st_transform(crs = 4326)
 
-  #message("Loading shapefile for ", ward, ", ", state)
+  message("Loading shapefile for ", ward, ", ", state)
   ward_shp <- st_read(ward_shapefile_path)  %>%
     st_transform(crs = 4326)
 
@@ -42,6 +43,9 @@ est_buildingheightpop <- function(building_data_path, settlement_data_path, ward
   residential_buildings <- st_join(building_data, settlement_blocks, join = st_within) %>%
     filter(landuse == "Residential") %>%
     filter(st_geometry_type(.) == "POLYGON")
+
+  # spatial join to transfer WardName from shapefile to residential_buildings
+  residential_buildings <- st_join(residential_buildings, ward_shp["WardName"], join = st_within)
 
   ## -----------------------------------------------------------------------------------------------------------------------------------------
   ### Read in raster building height data, process, and save as .csv for future use
@@ -70,15 +74,15 @@ est_buildingheightpop <- function(building_data_path, settlement_data_path, ward
     dplyr::select(id, FID, WardName, building_height) %>%
     st_drop_geometry()
 
-  write.csv(height_summary, file.path(output_dir, paste0(ward, "_buildings.csv")))
+  #write.csv(height_summary, file.path(output_dir, paste0(ward, "_buildings.csv")))
 
   ## -----------------------------------------------------------------------------------------------------------------------------------------
   ### Read in saved building height .csv
   ## -----------------------------------------------------------------------------------------------------------------------------------------
-  building_heights <- read.csv(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/Raster_files/GoogleBuildings2_5/Asaba/asaba_buildings.csv"))
+  #building_heights <- read.csv(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/Raster_files/GoogleBuildings2_5/Asaba/asaba_buildings.csv"))
 
   # convert height to likely household number per building
-  building_story_count <- building_heights %>%
+  building_story_count <- height_summary %>%
     dplyr::filter(building_height >= 1.8) %>%  # remove uninhabitable buildings less than one story (8015)
     dplyr::filter(building_height <= 14) %>%  # with average story height of 3.5m, remove buildings > 16m (3) or > 14m / 4 stories (7)
     mutate(resident_households = case_when(
@@ -116,7 +120,13 @@ est_buildingheightpop <- function(building_data_path, settlement_data_path, ward
                  values_to = "Population") %>%
     group_by(EstimateType) %>%
     mutate(percentage = (Population / sum(Population)) * 100) %>%
-    mutate(label = paste0(Population, " - ", round(percentage, 2), "%"))
+    mutate(label = paste0(Population, " (", round(percentage, 2), "%)")) %>%
+    mutate(EstimateType = recode(EstimateType,
+                                 "pop_estimate_uneven" = "Estimate Uneven",
+                                 "pop_estimate3" = "Estimate 3",
+                                 "pop_estimate4" = "Estimate 4",
+                                 "pop_estimate5" = "Estimate 5"
+    ))
 
   # create plot
   p1 <- ggplot(plot_all, aes(x = EstimateType, y = Population, fill = WardGroup))+
@@ -127,87 +137,8 @@ est_buildingheightpop <- function(building_data_path, settlement_data_path, ward
          x = "Household sizes",
          y = "Population",
          fill = "Ward Group")+
-    geom_text(aes(label = label), position = position_stack(vjust = 0.5), color = "white", size = 3) +
+    geom_text(aes(label = label), position = position_stack(vjust = 0.5), color = "black", size = 3) +
     theme_manuscript()
   p1
 
-  ## -----------------------------------------------------------------------------------------------------------------------------------------
-  ### Other
-  ## -----------------------------------------------------------------------------------------------------------------------------------------
-
-  # extract mean building heights
-  building_heights <- exactextractr::exact_extract(building_heights, residential_buildings)
-  mean_heights <- lapply(building_heights, function(df) mean(df$value, na.rm = TRUE))
-  residential_buildings$building_height <- unlist(mean_heights)
-
-  # set values for building height to likely household size per building
-  household_sizes = list(
-    "1.8-3.5" = 1,
-    "3.5-7.0" = 1.5,
-    "7.0-10.5" = 2,
-    "10.5-14.0" = 2.5
-  )
-
-  # filter and calculate the estimated number of resident households based on building height
-  filtered_buildings <- residential_buildings %>%
-    filter(building_height >= 1.8, building_height <= 14.0) %>%
-    mutate(resident_households = case_when(
-      building_height >= 1.8 & building_height < 3.5 ~ household_sizes[["1.8-3.5"]],
-      building_height >= 3.5 & building_height < 7.0 ~ household_sizes[["3.5-7.0"]],
-      building_height >= 7.0 & building_height < 10.5 ~ household_sizes[["7.0-10.5"]],
-      building_height >= 10.5 & building_height < 14.0 ~ household_sizes[["10.5-14.0"]]
-    ))
-
-  # summarize households counts
-  household_counts <- filtered_buildings %>%
-    #group_by(WardName) %>%
-    summarise(total_households = sum(resident_households, na.rm = TRUE)) %>%
-    mutate(total_households = ceiling(total_households))
-
-  # calculate population estimates
-  population_data <- household_counts %>%
-    mutate(WardType = case_when(
-      WardName %in% urban_wards ~ "Urban",
-      WardName %in% part_urban_wards ~ "Part-Urban",
-      TRUE ~ "Rural"
-    )) %>%
-    mutate(pop_estimate5 = total_households * 5,
-           pop_estimate4 = total_households * 4,
-           pop_estimate3 = total_households * 3) %>%
-    mutate(pop_estimate_uneven = case_when(
-      WardType == "Urban" ~ total_households * 3,
-      WardType == "Rural" ~ total_households * 5,
-      WardType == "Part-Urban" ~ total_households * 4
-    )) %>%
-    mutate(WardGroup = ifelse(WardName %in% low_risk_wards, WardName, "Rest of Wards")) %>%
-    group_by(WardGroup) %>%
-    summarize(pop_estimate5 = sum(pop_estimate5),
-              pop_estimate3 = sum(pop_estimate3),
-              pop_estimate4 = sum(pop_estimate4),
-              pop_estimate_uneven = sum(pop_estimate_uneven)) %>%
-    pivot_longer(cols = starts_with("pop_estimate"),
-                 names_to = "EstimateType",
-                 values_to = "Population") %>%
-    group_by(EstimateType) %>%
-    mutate(percentage = (Population / sum(Population)) * 100,
-           label = paste0(Population, " - ", round(percentage, 2), "%"))
-
-  # save summary to csv
-  message("Saving summary .csv file to: ", output_dir)
-  write.csv(filtered_buildings, output_dir, row.names = FALSE)
-
-  # create bar plot
-  population_plot <- ggplot(population_data, aes(x = EstimateType, y = Population, fill = WardGroup)) +
-    geom_bar(stat = "identity") +
-    scale_fill_manual(values = c("Rest of Wards" = "coral",
-                                 "Urban" = "pink",
-                                 "Part-Urban" = "lightyellow")) +
-    labs(title = "Population Estimates by Ward Group",
-         x = "Estimate Type",
-         y = "Population",
-         fill = "Ward Group") +
-    geom_text(aes(label = label), position = position_stack(vjust = 0.5)) +
-    theme_manuscript()
-
-  return(population_plot)
 }
